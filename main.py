e82b9201bf6a9ede32763ab28269c9c948e815b3
Konrad Klimaszewski <konrad.klimaszewski@ncbj.gov.pl>
2024-03-26 11:06:42 +0100

    Display

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 # Komentarz
 
 print("Git handson")
+
+DATA = [
+    {
+        "imię": "Filemon",
+        "gatunek": "Kot",
+        "waga": 1,
+        "wiek": 0.5
+    },
+    {
+        "imię": "Szarik",
+        "gatunek": "Pies",
+        "waga": 30,
+        "wiek": 2
+    }
+]
+
+def Display(dane):
+    print(dane)
+
+Display(DATA)
